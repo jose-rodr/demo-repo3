8e21db563dfeb7168a6dd5546d8bb9afaaac3948
Jose Gabriel <ethanbido@gmail.com>
2020-10-15 17:13:31 -0400

    Fixed merge conflicts (v2).

--- test.py
+++ test.py
@@ -1,25 +1,18 @@
 class Programmer:
     passion = 'Programming'
 
-<<<<<<< HEAD
-    def __init__(self, name, age, country, role, tech_stack):
-=======
-    def __init__(self, name, age, country, role, company):
->>>>>>> cbf335ca2f852d0ccfa846302d45fb5a3709c7c8
+    def __init__(self, name, age, country, role, company, tech_stack):
         self.name = name
         self.age = age
         self.country = country
         self.role = role
-<<<<<<< HEAD
         self.tech_stack = tech_stack
-=======
         self.company = company
->>>>>>> cbf335ca2f852d0ccfa846302d45fb5a3709c7c8
 
     def change_role(self, new_role):
         self.role = new_role
         return f'My new role is {self.role}.'
-    
+
     def say_age(self):
         return f'I am {self.age} years old.'
 
@@ -31,8 +24,6 @@ class Programmer:
     def greet():
         return f"Hi, I am a programmer."
 
-<<<<<<< HEAD
-jose = Programmer("José Gabriel", 16, "Dominican Republic", "Front-end developer", "MEAN")
-=======
-jose = Programmer("José Gabriel", 16, "Dominican Republic", "Front-end developer", "Google")
->>>>>>> cbf335ca2f852d0ccfa846302d45fb5a3709c7c8
+
+jose = Programmer("José Gabriel", 16, "Dominican Republic",
+                  "Front-end developer", "Google", "MEAN")
